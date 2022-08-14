d9de1a52402bb6f9d0f1e32b80bc28d40389c92d
Waleed Ahmed <waleed.wit@outlook.com>
2022-08-14 15:45:03 +0300

    gpsrow

--- C.Sharp/Program.cs
+++ C.Sharp/Program.cs
@@ -2,6 +2,7 @@
 
 
 
+using AutoMapper;
 using C.Sharp.Model;
 
 
@@ -17,7 +18,7 @@ var list = userList.OrderBy(x => x.GPSDate).ToList();
 
 foreach (var user in userList)
 {
-    //Console.WriteLine($"name{user.Name} date:{user.GPSDate}");
+    Console.WriteLine($"name{user.Name} date:{user.GPSDate}");
 }
 
 Console.WriteLine("Done.");
@@ -27,26 +28,16 @@ class Service
     public List<User> userList = new();
     public void InsertUser()
     {
-        userList.Add(new User() { Id = 1, Name = "User1", GPSDate = DateTime.Now.AddYears(2) });
-        userList.Add(new User() { Id = 3, Name = "User2", GPSDate = DateTime.Now.AddYears(20) });
-        userList.Add(new User() { Id = 4, Name = "User3", GPSDate = DateTime.Now.AddYears(10) });
-        userList.Add(new User() { Id = 5, Name = "User4", GPSDate = DateTime.Now.AddYears(-6) });
-        userList.Add(new User() { Id = 6, Name = "User5", GPSDate = DateTime.Now });
-        userList.Add(new User() { Id = 7, Name = "User6", GPSDate = DateTime.Now });
-        userList.Add(new User() { Id = 8, Name = "User7", GPSDate = DateTime.Now });
-        userList.Add(new User() { Id = 9, Name = "User8", GPSDate = DateTime.Now });
-        userList.Add(new User() { Id = 9, Name = "User9", GPSDate = DateTime.Now.AddYears(-5) });
-        userList.Add(new User() { Id = 10, Name = "User10", GPSDate = DateTime.Now.AddYears(1) });
-        userList.Add(new User() { Id = 1, Name = "User1", GPSDate = DateTime.Now.AddYears(2) });
+        userList.Add(new User() { Id = 7, Name = "User6", GPSDate = DateTime.Now.AddSeconds(50) });
+        userList.Add(new User() { Id = 8, Name = "User7", GPSDate = DateTime.Now.AddSeconds(90) });
+        userList.Add(new User() { Id = 9, Name = "User8", GPSDate = DateTime.Now.AddSeconds(160) });
+
         Console.WriteLine("add 10 users success.");
     }
     public User[] RemoveFutureDate(User[] usersList)
     {
-        var inFutureOrPast = usersList.Where(g => g.GPSDate.Subtract(DateTime.Now).TotalMinutes > 2 ||
-        g.GPSDate.Year < DateTime.Now.AddYears(-3).Year);
-
-        usersList = usersList.Except(inFutureOrPast).ToArray();
-
+        usersList = usersList.Where(g => g.GPSDate.Subtract(DateTime.Now).TotalMinutes <= 2 ||
+        g.GPSDate.Year >= DateTime.Now.AddYears(-3).Year).ToArray();
         return usersList;
     }
 }
